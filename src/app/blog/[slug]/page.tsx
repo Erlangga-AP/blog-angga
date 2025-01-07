fb653af850701014d15b30983b1e07e7e906a84e
Erlangga--AP <erlanggaadiprasatya2005@gmail.com>
2025-01-08 01:46:18 +0700

    masih ada kesalahan di slug blog dan search

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,77 +1,82 @@
-import { getSingleBlogPost } from "@/utils/contentful-data";
-import Image from "next/image";
-import { BLOCKS, Document } from "@contentful/rich-text-types";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+// import { getSingleBlogPost } from "@/utils/contentful-data";
+// import Image from "next/image";
+// import { BLOCKS, Document } from "@contentful/rich-text-types";
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-interface BlogPost {
-  blogImage: string;
-  title: string;
-  content: Document;
-}
+// interface BlogPost {
+//   blogImage: string;
+//   title: string;
+//   content: Document;
+// }
 
-interface Params {
-  slug: string;
-}
+// // Tidak perlu mendefinisikan tipe Params secara manual.
+// export default async function DetailPage({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   const slug = params.slug;
 
-export default async function DetailPage({ params }: { params: Params }) {
-  const slug = params.slug;
+//   // Ambil data post
+//   const post = (await getSingleBlogPost(slug)) as unknown as BlogPost;
 
-  // Ambil data post
-  const post = (await getSingleBlogPost(slug)) as unknown as BlogPost;
+//   // Tangani jika data tidak ditemukan
+//   if (!post) {
+//     return (
+//       <section className="flex items-center justify-center min-h-screen">
+//         <h1 className="text-2xl font-bold">Blog post not found</h1>
+//       </section>
+//     );
+//   }
 
-  // Tangani jika data tidak ditemukan
-  if (!post) {
-    return (
-      <section className="flex items-center justify-center min-h-screen">
-        <h1 className="text-2xl font-bold">Blog post not found</h1>
-      </section>
-    );
-  }
+//   return (
+//     <section className="bg-white font-LibreFranklin">
+//       <div className="pt-10 flex flex-col items-center">
+//         <div className="relative h-[300px] lg:h-[450px] lg:w-[700px] max-w-4xl grid justify-items-center">
+//           <Image
+//             src={post.blogImage}
+//             alt="image"
+//             fill
+//             className="object-cover"
+//           />
+//         </div>
 
-  return (
-    <section className="bg-white font-LibreFranklin">
-      <div className="pt-10 flex flex-col items-center">
-        <div className="relative h-[300px] lg:h-[450px] lg:w-[700px] max-w-4xl grid justify-items-center">
-          <Image
-            src={post.blogImage}
-            alt="image"
-            fill
-            className="object-cover"
-          />
-        </div>
+//         <h1 className="text-[32px] sm:text-[40px] md:text-[50px] pt-5 font-bold text-center max-w-4xl px-6">
+//           {post.title}
+//         </h1>
 
-        <h1 className="text-[32px] sm:text-[40px] md:text-[50px] pt-5 font-bold text-center max-w-4xl px-6">
-          {post.title}
-        </h1>
+//         <div className="px-6 sm:px-12 md:px-36 pb-20">
+//           {documentToReactComponents(post.content, {
+//             renderNode: {
+//               [BLOCKS.HEADING_2]: (node, children) => {
+//                 return (
+//                   <h2 className="font-semibold mt-9 text-2xl sm:text-xl md:text-2xl">
+//                     {children}
+//                   </h2>
+//                 );
+//               },
+//               [BLOCKS.PARAGRAPH]: (node, children) => {
+//                 return (
+//                   <p className="mt-4 text-base sm:text-lg md:text-xl font-normal">
+//                     {children}
+//                   </p>
+//                 );
+//               },
+//               [BLOCKS.UL_LIST]: (node, children) => {
+//                 return (
+//                   <ul className="list-disc pl-7 text-lg sm:text-xl md:text-2xl font-bold text-[#494A53]">
+//                     {children}
+//                   </ul>
+//                 );
+//               },
+//             },
+//           })}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
 
-        <div className="px-6 sm:px-12 md:px-36 pb-20">
-          {documentToReactComponents(post.content, {
-            renderNode: {
-              [BLOCKS.HEADING_2]: (node, children) => {
-                return (
-                  <h2 className="font-semibold mt-9 text-2xl sm:text-xl md:text-2xl">
-                    {children}
-                  </h2>
-                );
-              },
-              [BLOCKS.PARAGRAPH]: (node, children) => {
-                return (
-                  <p className="mt-4 text-base sm:text-lg md:text-xl font-normal">
-                    {children}
-                  </p>
-                );
-              },
-              [BLOCKS.UL_LIST]: (node, children) => {
-                return (
-                  <ul className="list-disc pl-7 text-lg sm:text-xl md:text-2xl font-bold text-[#494A53]">
-                    {children}
-                  </ul>
-                );
-              },
-            },
-          })}
-        </div>
-      </div>
-    </section>
-  );
+export default function DetailPage() {
+  return <section>testing</section>;
 }
